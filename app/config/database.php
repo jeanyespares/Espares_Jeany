cd266239ffe5958eba4b233ec239a4a235b199ae
jeanyespares <jeanyespares404@gmail.com>
2025-09-15 15:37:56 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'jeany',
-    'password'	=> 'jeany',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12798704',
+    'password'	=> 'atRFR1l5Pu',
+    'database'	=> 'sql12798704',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
